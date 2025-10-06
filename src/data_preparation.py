1edfbcffa7a765bcc6846814bc9b43770526fefa
KiriLLchik07 <kirill3456777@mail.ru>
2025-10-06 23:17:14 +0600

    refactor: add method, that save train and test data. Fill the notebook

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 from imblearn.over_sampling import SMOTE
+import joblib
+import os
 
 class PrepareData:
     def __init__(self, df):
@@ -11,6 +13,7 @@ class PrepareData:
         self.y_test = None
         self.X_train_balanced = None
         self.y_train_balanced = None
+        self.scaler = None
 
     def splitting(self):
         X = self.df.drop(columns=['Exited'])
@@ -26,16 +29,16 @@ class PrepareData:
         return self.X_train, self.X_test, self.y_train, self.y_test
     
     def scaling(self):
-        scaling = RobustScaler()
+        self.scaler = RobustScaler()
 
         numeric_features = ['CreditScore', 'Age', 'Balance', 'EstimatedSalary']
 
         numeric_features = [col for col in self.X_train.columns if self.X_train[col].nunique() > 2 
                             & self.X_train[col].nunique() !=4]
 
-        self.X_train[numeric_features] = scaling.fit_transform(self.X_train[numeric_features])
+        self.X_train[numeric_features] = self.scaler.fit_transform(self.X_train[numeric_features])
 
-        self.X_test[numeric_features] = scaling.transform(self.X_test[numeric_features])
+        self.X_test[numeric_features] = self.scaler.transform(self.X_test[numeric_features])
 
         return self.X_train, self.X_test
     
@@ -46,10 +49,25 @@ class PrepareData:
 
         return self.X_train_balanced, self.y_train_balanced
     
+        
+    def save_to_pickle(self, output_dir='../data/processed'):
+        os.makedirs(output_dir, exist_ok=True)
+        
+        joblib.dump(self.X_train_balanced, f'{output_dir}/X_train.pkl')
+        joblib.dump(self.X_test, f'{output_dir}/X_test.pkl')
+        joblib.dump(self.y_train_balanced, f'{output_dir}/y_train.pkl')
+        joblib.dump(self.y_test, f'{output_dir}/y_test.pkl')
+        
+        joblib.dump(self.scaler, f'{output_dir}/scaler.pkl')
+        joblib.dump(self.X_train.columns.tolist(), f'{output_dir}/feature_names.pkl')
+        
+        print(f"✅ Данные и объекты сохранены в {output_dir}")
+    
     def preparing(self):
 
         self.splitting()
         self.scaling()
         self.balancing_classes()
+        self.save_to_pickle()
 
         return self.X_train_balanced, self.X_test, self.y_train_balanced, self.y_test
