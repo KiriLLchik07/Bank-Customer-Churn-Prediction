e0722d896289de6afcf644a5a6a07032bca1cc06
KiriLLchik07 <kirill3456777@mail.ru>
2025-10-09 17:38:08 +0600

    feat: add plot-curve function, display report in notebook

--- src/model_training.py
+++ src/model_training.py
@@ -1,6 +1,12 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score, f1_score, classification_report, precision_score, recall_score, roc_curve, auc
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
+from xgboost import XGBClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_auc_score, f1_score, classification_report
+import matplotlib.pyplot as plt
 
 class TrainModels:
     def __init__(self, X_train, X_test, y_train, y_test):
@@ -8,30 +14,69 @@ class TrainModels:
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
-        self.baseline_models = {}
+        self.models = {}
         self.predictions = {}
-
-    def fit_baseline_models(self):
+    
+    def fit_models(self,):
         logistic_regression = LogisticRegression(random_state=42, max_iter=1000)
         logistic_regression.fit(self.X_train, self.y_train)
-        self.baseline_models['LogisticRegression'] = logistic_regression
+        self.models['LogisticRegression'] = logistic_regression
+
+        knn = KNeighborsClassifier()
+        knn.fit(self.X_train, self.y_train)
+        self.models['KNeighborsClassifier'] = knn
+
+        decision_tree = DecisionTreeClassifier(random_state=42)
+        decision_tree.fit(self.X_train, self.y_train)
+        self.models['DecisionTreeClassifier'] = decision_tree
 
         random_forest = RandomForestClassifier(random_state=42)
         random_forest.fit(self.X_train, self.y_train)
-        self.baseline_models["RandomForestClassifier"] = random_forest
+        self.models['RandomForestClassifier'] = random_forest
 
-        return self.baseline_models
+        xgboost = XGBClassifier(random_state=42)
+        xgboost.fit(self.X_train, self.y_train)
+        self.models['XGBClassifier'] = xgboost
+        
+        lightgbm = LGBMClassifier(random_state=42, verbose=0)
+        lightgbm.fit(self.X_train, self.y_train)
+        self.models['LGBMClassifier'] = lightgbm
 
-    def evaluate(self):
-        for name_model, model in self.baseline_models.items():
+        catboost = CatBoostClassifier(random_state=42, verbose=0)
+        catboost.fit(self.X_train, self.y_train)
+        self.models['CatBoostClassifier'] = catboost
+
+        return self.models
+    
+    def evaluate_models(self):
+        for name_model, model in self.models.items():
             y_pred = model.predict(self.X_test)
             y_pred_proba = model.predict_proba(self.X_test)[:, 1]
             self.predictions[name_model] = {
                 "roc_auc_score": roc_auc_score(self.y_test, y_pred_proba),
                 "f1_score": f1_score(self.y_test, y_pred),
+                "precision": precision_score(self.y_test, y_pred),
+                "recall": recall_score(self.y_test, y_pred),
                 "classification_report": classification_report(self.y_test, y_pred)
             }
 
         return self.predictions
+    
+    def plot_roc_curve(self):
 
+        for name_model, model in self.models.items():
+            y_pred_proba = model.predict_proba(self.X_test)[:,1]
+        
+            fpr, tpr, _ = roc_curve(self.y_test, y_pred_proba)
+            roc_auc = auc(fpr, tpr)
 
+            plt.plot(fpr, tpr, lw=2,
+            label=f'{name_model} (AUC = {roc_auc:.3f})')
+
+            plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title(f'ROC Curves for {name_model}')
+            plt.legend()
+            plt.grid(True)
+            plt.show()
