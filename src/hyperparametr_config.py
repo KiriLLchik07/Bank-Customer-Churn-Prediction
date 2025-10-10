4ae0955d5061b967fc183a8109660a15dc1d337b
KiriLLchik07 <kirill3456777@mail.ru>
2025-10-10 16:16:48 +0600

    feat: base selection of hyperparameters is complete. Search the threshold for f1 metric

--- src/hyperparametr_config.py
+++ src/hyperparametr_config.py
@@ -1,9 +1,9 @@
 MODEL_PARAMS_CONFIG = {
     'catboost': {
         'class': 'CatBoostClassifier',
-        'fixed_params': {'random_state': 42, 'verbose': False},
+        'fixed_params': {'random_state': 42, 'verbose': False, 'auto_class_weights': 'Balanced'},
         'grid_params': {
-            'iterations': {'type': 'int', 'low': 500, 'high': 1250},
+            'iterations': {'type': 'int', 'low': 200, 'high': 1000},
             'depth': {'type': 'int', 'low': 4, 'high': 8},
             'learning_rate': {'type': 'float', 'low': 0.01, 'high': 0.3, 'log': True},
             'l2_leaf_reg': {'type': 'float', 'low': 1, 'high': 10},
@@ -14,9 +14,9 @@ MODEL_PARAMS_CONFIG = {
     },
     'lightgbm': {
         'class': 'LGBMClassifier', 
-        'fixed_params': {'random_state': 42, 'verbose': -1},
+        'fixed_params': {'random_state': 42, 'verbose': -1, 'class_weight': 'balanced'},
         'grid_params': {
-            'n_estimators': {'type': 'int', 'low': 100, 'high': 1250},
+            'n_estimators': {'type': 'int', 'low': 100, 'high': 1000},
             'max_depth': {'type': 'int', 'low': 3, 'high': 8},
             'learning_rate': {'type': 'float', 'low': 0.01, 'high': 0.3, 'log': True},
             'num_leaves': {'type': 'int', 'low': 20, 'high': 100},
@@ -31,7 +31,7 @@ MODEL_PARAMS_CONFIG = {
         'class': 'XGBClassifier',
         'fixed_params': {'random_state': 42},
         'grid_params': {
-            'n_estimators': {'type': 'int', 'low': 100, 'high': 1250},
+            'n_estimators': {'type': 'int', 'low': 100, 'high': 1000},
             'max_depth': {'type': 'int', 'low': 3, 'high': 8},
             'learning_rate': {'type': 'float', 'low': 0.01, 'high': 0.3, 'log': True},
             'subsample': {'type': 'float', 'low': 0.6, 'high': 1.0},
