c094f4e3e1a6954041d8772cabe7b67e4f3d67cb
KiriLLchik07 <kirill3456777@mail.ru>
2025-10-21 23:28:31 +0600

    feat: create base view

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-page_1 = st.Page("pages/about_project.py", title="Главная страница", icon="❄️")
-page_2 = st.Page("pages/EDA.py", title="EDA", icon="🎉")
-page_3 = st.Page("pages/modeling.py", title="Моделирование и эксперименты", icon="❄️")
-page_4 = st.Page("pages/interpretation.py", title="Интерпретация модели", icon="🎉")
-page_5 = st.Page("pages/predictions.py", title="Предсказать отток", icon="❄️")
+page_1 = st.Page("pages/about_project.py", title="Главная страница", icon="🏡")
+page_2 = st.Page("pages/EDA.py", title="EDA", icon="📊")
+page_3 = st.Page("pages/modeling.py", title="Моделирование и эксперименты", icon="🧪")
+page_4 = st.Page("pages/interpretation.py", title="Интерпретация модели", icon="📝")
+page_5 = st.Page("pages/predictions.py", title="Предсказать отток", icon="🔮")
 
 pg = st.navigation([ page_1, page_2, page_3, page_4, page_5])
 
